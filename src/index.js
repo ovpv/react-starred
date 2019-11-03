f1b3ab76ee9936d302967fece95a3b74dab98b40
ovpv <viktor.prasad@gmail.com>
2019-11-03 11:41:32 +0530

    :fire: removed unwanted code from index.js file

--- src/index.js
+++ src/index.js
@@ -1,15 +1,3 @@
 import Stars from "./components/stars";
-import React from "react";
-import { render } from "react-dom";
-
-const App = () => {
-	return (
-		<div>
-			<Stars given={3} on={10} />
-		</div>
-	);
-};
-
-render(<App />, document.getElementById("app"));
 
 export default Stars;
