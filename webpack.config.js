df1df0ff15e1f6f1a009efedc11b73ad1caec79e
ovpv <viktor.prasad@gmail.com>
2019-11-03 18:44:58 +0530

    :wrench: made changes to webpack and package file to build module as a library component

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,8 @@ module.exports = [
 		mode: "production",
 		output: {
 			path: path.resolve(__dirname, "public"),
-			filename: "[name].js"
+			filename: "[name].js",
+			libraryTarget: "commonjs2"
 		},
 		module: {
 			rules: [
@@ -23,6 +24,9 @@ module.exports = [
 					}
 				}
 			]
+		},
+		externals: {
+			react: "commonjs react" // this line is just to use the React dependency of our parent-testing-project instead of using our own React.
 		}
 	}
 ];
